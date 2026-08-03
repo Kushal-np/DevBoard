19099c465d345ab61087b8a63620c16358c52348
kushal-np <wrckushal@gmail.com>
2026-08-03 22:12:36 +0545

    Cookie parser added - for production issue fixing

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,6 +28,7 @@ app.use(
 )
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
 app.use("/api/user" , userRoutes);
 app.use("/api/profile" , profileRoutes );
 app.use("/api/project" , projectRoutes )
