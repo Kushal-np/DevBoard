20e257ecf11029a7186249c57ad6b6fc078b798c
kushal-np <wrckushal@gmail.com>
2026-08-03 22:22:05 +0545

    Socket origin configured for client side

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ initChatSocket(server);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://dev-board-ej2n1587y-kushal-poudels-projects.vercel.app",
     credentials: true,
   })
 )
